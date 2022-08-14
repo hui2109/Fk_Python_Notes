06a3da7f761e1dd86d72ad21342828fb0f1599c2
辉哥 <995631664@qq.com>
2022-08-14 09:47:15 +0800

    更新3_data_test.py

--- 15_网络编程/3_data_test.py
+++ 15_网络编程/3_data_test.py
@@ -1,4 +1,4 @@
 from urllib.request import *
 
-with urlopen('http://localhost:8080/test/test','测试数据'.encode('utf-8')) as f:
+with urlopen('http://localhost:8080/test/test', data='测试数据'.encode('utf-8')) as f:
     print(f.read().decode('utf-8'))
